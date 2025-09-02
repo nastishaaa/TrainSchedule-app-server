aa8933c464b621c43ec4cef99fe92e5569329c8d
Anastasiia <anastasiiprfnk@gmail.com>
2025-09-02 19:22:36 +0300

    connection string add

--- src/db-init.ts
+++ src/db-init.ts
@@ -4,12 +4,9 @@ import { Pool } from "pg";
 dotenv.config();
 
 export const dbPool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: Number(process.env.POSTGRES_PORT),
+  connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}.oregon-postgres.render.com/${process.env.POSTGRES_DB}`,
 });
+
 
 const initDb = async (): Promise<void> => {
   try {
